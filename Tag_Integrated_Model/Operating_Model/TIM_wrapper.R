3ccbd7a874fbaada6e25b57b3fdedb9f3e01ea66
KatelynBosley <katelyn.bosley@noaa.gov>
2017-09-08 13:02:53 -0700

    New features for R wrapper
    Wrapper now completes calculations to convert tag-recap proportions generated from multinomial to counts by dividing proportions by N_tags for each release. Also, generates arrays for SE of catch and survey instead of CV. Eventually, move these calcs to ADMB.

--- Tag_Integrated_Model/Operating_Model/TIM_wrapper.R
+++ Tag_Integrated_Model/Operating_Model/TIM_wrapper.R
@@ -48,10 +48,15 @@ nyrs<-out$nyrs #simulation years
 npops<-out$npops #populations
 nregions<-out$nregions #regions - thankfully only 1 per pop
 nfleets<-out$nfleets #nfleets fishery
-nfleets_survey<-out$nfleets_survey #nfleets survey (usually 1)
+nfleets_survey<-out$nfleets_survey #nfleets survey (usually 1 for this model)
 tag_rel_years<-out$years_of_tag_releases # model years with tag releases
 n_rel<-length(tag_rel_years) #number of releases
 max_life_tags<-out$max_life_tags # number of years tally tags after release - need to output this in OM
+
+#to adjust tag number to the conversion - in the case of SAB the abundance is in thousands of fish so the number of tags is muliplied by the conversion - 1000. May need to add this to ADMB.
+n_conv<-1000
+
+
 
 #setting everything as a .csv for now to check dims and values
 
@@ -76,17 +81,22 @@ write.csv(OBS_survey_region_bio,"OBS_survey_bio.csv")
 
 #setting up the obs_survey_fleet_se
 #create a matrix with dimensions (1,np,1,nreg,1,ny,1,nfs)
-OBS_survey_region_bio_se<-matrix(NA,(npops*nregions*nfleets),nyrs)
+OBS_survey_region_bio_se_temp<-matrix(NA,(npops*nregions*nfleets),nyrs)
 for(i in 1:nyrs){
-  OBS_survey_region_bio_se[,i]<-out$sigma_survey
+  OBS_survey_region_bio_se_temp[,i]<-out$sigma_survey
 }
-write.csv(OBS_survey_region_bio_se,"OBS_survey_region_bio_se.csv")
+
+OBS_survey_region_bio_se<-OBS_survey_region_bio*OBS_survey_region_bio_se_temp
+
+#write.csv(OBS_survey_region_bio_se,"OBS_survey_region_bio_se.csv")
+
 
 #Setting up the age compositions. Needs to be (1,np,1,nreg,1,ny,1,nfs,1,na). 
 temp<-grep("alt_OBS_survey_prop", names(out), value = TRUE) #pulling out the age_comps
 temp2<-out[temp] 
 OBS_survey_prop<-do.call("rbind", temp2)
 write.csv(OBS_survey_prop,"OBS_survey_prop.csv")
+
 
 #setting up the survey_sample N
 #create a matrix with dimensions (1,np,1,nreg,1,ny,1,nfs)
@@ -102,16 +112,20 @@ write.csv(OBS_survey_prop_N,"OBS_survey_prop_N.csv")
 
 #yield catch matrix
 OBS_yield_region<-t(out$OBS_yield_region)
-write.csv(OBS_yield_region,"OBS_yield_fleet.csv")
+#write.csv(OBS_yield_region,"OBS_yield_fleet.csv")
+
 
 #yield_se
-OBS_yield_fleet_se<-matrix(NA,(npops*nregions*nfleets),nyrs)
+OBS_yield_fleet_temp<-matrix(NA,(npops*nregions*nfleets),nyrs)
 for(i in 1:nyrs){
-  OBS_yield_fleet_se[,i]<-out$sigma_catch
+  OBS_yield_fleet_temp[,i]<-out$sigma_catch
 }
-write.csv(OBS_yield_fleet_se,"OBS_yield_fleet_se.csv")
 
-#fishery age compositions
+OBS_yield_fleet_se<-OBS_yield_fleet_temp * OBS_yield_region
+
+write.csv(OBS_yield_fleet_se,"OBS_yield_fleet_se.csv") 
+
+#fishery age compositions #combining observed catches from pops
 temp<-grep("alt_OBS_catch_prop", names(out), value = TRUE) #pulling out the age_comps
 temp2<-out[temp] 
 OBS_catch_at_age_fleet_prop<-do.call("rbind", temp2)
@@ -127,15 +141,10 @@ write.csv(OBS_catch_prop_N,"OBS_catch_prop_N.csv")
 #####################
 # TAGGING DATA
 #####################
+n_tags_total<-floor(out$ntags_total*1000) # not fully getting this tag release numbers
 
 #ntags (1,np,1,nreg,1,ny_rel,1,na) #number of tags released in each year for each age
-ntags<-out$ntags_matrix # this will be tricky
-#not generallized #SET UP FOR 3 POPS/AREA
-#temp1<-(ntags[1:years_tally])
-#temp2<-(ntags[(years_tally+1):(years_tally*2),])
-#temp3<-(ntags[((years_tally*2)+1):(years_tally*3),])
-#ntags_matrix<-(rbind(temp1,temp2,temp3))
-
+ntags<-floor(out$ntags_matrix*n_conv) ##to adjust for thousands of fish tagged
 write.csv(ntags,"ntags_matrix.csv")
 
 #backup plan  = set number of tags manually
@@ -153,10 +162,12 @@ temp2<-out[temp] #pulling all the recapture
 #proportion - full matrix
 temp1<-grep("OBS_tag_prop_final", names(out), value = TRUE) #pulling out the recaptures
 temp1.1<-out[temp1] #pulling all the recapture
-recaps<-do.call("rbind", temp1.1) # if many populations
+recaps<-(do.call("rbind", temp1.1))/2 # For sum reason this is summing to 2 and 1...not sure why...
 write.csv(recaps,"tag_recaps.csv")
 
+# now converting to the total released
 
+#yikes...doing the math now
 temp2<-grep("OBS_tag_prop_reg", names(out), value = TRUE)
 length(temp2)
 temp2.1<-out[temp2] #pulling all the recaptures
@@ -176,18 +187,37 @@ test.array<-array(NA,dim=c(nages,max_life_tags*nregions+1,index)) #3d array
  test.array<-test.array[,1:15,]
  dim(test.array)
  
- #split the array into three separate
  
+ #split the array into three separate parts for calcs
  recap1<-test.array[,1:5,] #recoveries for reg1
- recap2<-test.array[,6:10,] #recoveries in reg2
- recap3<-test.array[,11:15,] #recoveries in reg3
  
+ #set up temp for calculating tagN
+ recap_counts1<-recap1
+ for(i in 1:index) {
+   recap_counts1[,,i]<-recap1[,,i]*ntags[i,]
+ }
+ 
+ recap2<-test.array[,6:10,] #recoveries in reg2
+ recap_counts2<-recap2
+ for(i in 1:index) {
+   recap_counts2[,,i]<-recap2[,,i]*ntags[i,]
+ }
+ 
+ recap3<-test.array[,11:15,] #recoveries in reg3
+ recap_counts3<-recap3
+ for(i in 1:index) {
+   recap_counts3[,,i]<-recap3[,,i]*ntags[i,]
+ }
+ 
+ 
+ 
+#filling the new array with counts
  new.array1<-array(NA,dim=c(max_life_tags,1,index,nages)) # think I have it...now filling it
  
  for(i in 1:max_life_tags){
    for(j in 1:index){
      for(k in 1:nages){
-        new.array1[i,1,j,k]<-recap1[k,i,j]
+        new.array1[i,1,j,k]<-recap_counts1[k,i,j]
  
  }}}
  
@@ -196,7 +226,7 @@ test.array<-array(NA,dim=c(nages,max_life_tags*nregions+1,index)) #3d array
  for(i in 1:max_life_tags){
    for(j in 1:index){
      for(k in 1:nages){
-       new.array2[i,1,j,k]<-recap2[k,i,j]
+       new.array2[i,1,j,k]<-recap_counts2[k,i,j]
        
      }}}
  
@@ -205,16 +235,16 @@ test.array<-array(NA,dim=c(nages,max_life_tags*nregions+1,index)) #3d array
  for(i in 1:max_life_tags){
    for(j in 1:index){
      for(k in 1:nages){
-       new.array3[i,1,j,k]<-recap3[k,i,j]
+       new.array3[i,1,j,k]<-recap_counts3[k,i,j]
        
      }}}
  
 #combine arrays
 tag.matrix.EM<-data.frame(reg1=as.vector(new.array1),reg2=as.vector(new.array2),reg3=as.vector(new.array3))
  
+#convert to integers
+tag.matrix.EM<-floor(tag.matrix.EM)
 write.csv(tag.matrix.EM,"obs_tag_array.csv")
-
-
 
 # getting true recaps from 7 d matrix from the report file
 recaps_all<-grep("recaps", names(out), value = TRUE) #pulling out the recaptures
